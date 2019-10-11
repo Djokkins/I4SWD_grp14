380d9c5f9cf368ab81a31ad86028baf534008f45
Andreas Hervert Nielsen <au570003@uni.au.dk>
2019-10-11 13:25:28 +0200

    Added all the classes

---
+++ Composite/Schoole.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Composite
+{
+    class Schoole
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
